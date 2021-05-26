85012fe549829471d8212d803e34b4c41b0a333b
Kabir Sing <kabirsing23423323@gmail.com>
2021-05-26 13:23:09 +0600

    fix admin.py

--- registation/admin.py
+++ registation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserTrack
 
-# Register your models here.
+class UserTrackAdmin(admin.ModelAdmin):
+    list_display = ['fname', 'lname', 'gender', 'phone_number', 'email', 'nid']
+    list_display_links = ['fname', 'lname']
+    list_editable = ['gender']
+    list_filter = ['gender']
+    list_per_page = 20
+
+
+admin.site.register(UserTrack, UserTrackAdmin)
